615787180290a6cba083be5e28c341fb350b5c02
yuvalMor27 <moryuval27@gmail.com>
2024-05-07 14:13:17 +0300

    q5 bonus initial commit

--- algorithmic_python/q5_pi.py
+++ algorithmic_python/q5_pi.py
@@ -1,1 +1,10 @@
-def reverse_n_pi_digits(n):
+import math
+
+
+def reverse_n_pi_digits(n) -> str:
+    num_pi = str(math.pi)[2:2+n]
+    return num_pi[::-1]
+
+
+if __name__ == '__main__':
+    print(reverse_n_pi_digits(5))
